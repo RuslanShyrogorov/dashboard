c78126d38dfb067d1f8a4fa9c0f497434676e117
Ruslan Shyrogorov <ruslanshirogorov@gmail.com>
2023-09-24 13:36:26 +0300

    finished Calendar, Kanban, ColorPicker, Editor pages

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import {
+  RichTextEditorComponent,
+  Inject,
+  HtmlEditor,
+  Image,
+  Link,
+  Toolbar,
+  QuickToolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+
+import { Header } from "../components";
 
 const Editor = () => {
-  return <div>Editor Editor</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent>
+        <Inject services={[HtmlEditor, Image, Link, Toolbar, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
 };
 
 export default Editor;
